bc9f6b936bb31dad9590a761889025f64bc85ecc
GkChris <gkagkaschristos@gmail.com>
2023-05-12 23:03:57 +0300

    init

---
+++ src/config/index.js
@@ -0,0 +1,2 @@
+export { default as appConfigurations } from './appConfigurations.json';
+export { default as externalURLs } from './externalURLs.json';
---
+++ src/data/index.js
@@ -0,0 +1,3 @@
+export { default as currencies } from './currencies.json';
+export { default as customCodes } from './customCodes.json';
+export { default as statusCodes } from './statusCodes.json';
