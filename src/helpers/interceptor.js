e955514d62e6203a44598af70b2bb6492dbe43df
GkChris <gkagkaschristos@gmail.com>
2023-07-03 10:56:28 +0300

    User is now saved on session storage

--- src/helpers/interceptor.js
+++ src/helpers/interceptor.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+
 export const ReqInterceptor = () => {
     axios.interceptors.request.use((req) => {
         const authToken = Cookies.get('authorization');
@@ -13,9 +14,7 @@ export const ReqInterceptor = () => {
 
 export const ResInterceptor = () => {
     axios.interceptors.response.use((res) => {
-
-        // Write the code to store user informations on the browser
-
+        if ( res.data?.data?.user ) sessionStorage.setItem('key', JSON.stringify(res.data.data.user));
         return res;
     })
 };  
