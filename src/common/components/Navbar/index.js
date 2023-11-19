5a29623def8a6c4380d967e1e020971006a7fc55
GkChris <gkagkaschristos@gmail.com>
2023-11-19 20:46:24 +0200

    Added style to navbar

--- src/common/components/Navbar/index.js
+++ src/common/components/Navbar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Cookies from 'js-cookie';
 import browserDatabaseKeys from '../../../config/browserDatabaseKeys'
 import { useAuthApi } from '../../../setup/authentication/requests';
+import './style.css';
 
 const authenticationCookieKey = browserDatabaseKeys?.cookies?.authorization;
 
@@ -10,6 +11,11 @@ const Navbar = () => {
 
     const authApiCalls = useAuthApi();
     const isLogged = Cookies.get(authenticationCookieKey);
+
+
+    const brand_click_action = () => {
+		window.location.assign('/')
+    }
 
     const logout_user = () => {
         authApiCalls.logout()
@@ -27,17 +33,17 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className="brand">Your Brand Name</div>
-      <div className="nav-buttons">
-        {isLogged ? (
-          <button onClick={() => logout_user()}>Logout</button>
-        ) : (
-          <>
-            <button onClick={() => login_user()}>Login</button>
-            <button onClick={() => register_user()}>Register</button>
-          </>
-        )}
-      </div>
+		<div className="brand" onClick={() => brand_click_action()}>Your Brand Name</div>
+		<div className="nav-buttons">
+			{isLogged ? (
+				<button onClick={() => logout_user()}>Logout</button>
+			) : (
+				<>
+					<button onClick={() => login_user()}>Login</button>
+					<button onClick={() => register_user()}>Register</button>
+				</>
+			)}
+		</div>
     </nav>
   );
 };
